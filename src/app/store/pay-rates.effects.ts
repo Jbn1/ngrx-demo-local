836969aab4ffccd6e7aed7a0063bbcfc299ec41b
Jbn1 <brianjulien@live.fr>
2022-09-25 10:44:31 +0200

    ConcatLatestFrom example

--- src/app/store/pay-rates.effects.ts
+++ src/app/store/pay-rates.effects.ts
@@ -1,13 +1,17 @@
 import {PayRatesService} from "../services/pay-rates.service";
-import {loadPayRates, loadPayRatesSuccess} from "./pay-rates.actions";
-import {Actions, createEffect, ofType} from "@ngrx/effects";
+import { loadPayRates, loadPayRatesSuccess, addOrUpdatePayRate } from './pay-rates.actions';
+import {Actions, concatLatestFrom, createEffect, ofType} from "@ngrx/effects";
 import {catchError, EMPTY, switchMap} from "rxjs";
-import {map} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 import {Injectable} from "@angular/core";
+import { Store } from "@ngrx/store";
+import { selectHighestPayRate } from './pay-rates.selector';
 
 @Injectable()
 export class PayRatesEffects {
-  constructor(private actions$: Actions, private payRatesService: PayRatesService) {}
+  constructor(private actions$: Actions,
+    private store: Store,
+    private payRatesService: PayRatesService) {}
 
   loadPayRates$ = createEffect(() =>
     this.actions$.pipe(
@@ -20,4 +24,11 @@ export class PayRatesEffects {
       )
     )
   );
+
+  maxPayRates$ = createEffect(() =>
+  this.actions$.pipe(
+    ofType(addOrUpdatePayRate),
+    concatLatestFrom(() => this.store.select(selectHighestPayRate)),
+    tap(([, highest]) => console.log(highest?.value))
+  ), {dispatch: false});
 }
